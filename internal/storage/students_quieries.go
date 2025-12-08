1004dd67396abd9b76e1374fe92db66f08bfec34
YoungSusanoo <ksovfox@mail.ru>
2025-12-08 09:43:56 +0300

    Fix students queries

--- internal/storage/students_quieries.go
+++ internal/storage/students_quieries.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (s *Storage) GetStudents() ([]models.Student, error) {
-	query := `SELECT people.id, first_name, last_name, father_name, g.name FROM people, groups g WHERE type = 'S' AND g.id = group_id`
+	query := `SELECT * FROM students_with_group`
 	rows, err := s.pool.Query(context.Background(), query)
 	if err != nil {
 		return nil, err
@@ -17,28 +17,29 @@ func (s *Storage) GetStudents() ([]models.Student, error) {
 	return pgx.CollectRows(rows, pgx.RowToStructByPos[models.Student])
 }
 
-func (s *Storage) InsertStudent(students models.Student) error {
+func (s *Storage) InsertStudent(student models.Student) error {
 	query := `insert into students_with_group (first_name, last_name, father_name, name) values($1, $2, $3, $4);`
 	_, err := s.pool.Exec(
 		context.Background(),
 		query,
-		students.FirstName,
-		students.LastName,
-		students.FatherName,
-		students.Group,
+		student.FirstName,
+		student.LastName,
+		student.FatherName,
+		student.Group,
 	)
 	return err
 }
 
 func (s *Storage) UpdateStudent(student, studentNew models.Student) error {
-	query := `UPDATE people SET first_name = $1, last_name = $2, father_name = $3 WHERE id = $4`
+	query := `UPDATE students_with_group SET first_name = $1, last_name = $2, father_name = $3, name = $4 WHERE id = $5`
 	_, err := s.pool.Exec(
 		context.Background(),
 		query,
 		studentNew.FirstName,
 		studentNew.LastName,
 		studentNew.FatherName,
-		studentNew.Id,
+		studentNew.Group,
+		student.Id,
 	)
 	return err
 }
